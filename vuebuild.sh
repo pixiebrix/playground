7e8a7d78d55f017c991a1e43dbd93d104f8d3a7a
Emmanuel Obua <eobua6882@gmail.com>
2021-12-07 12:24:33 +0300

    Install npm and run build:prod

--- vuebuild.sh
+++ vuebuild.sh
@@ -1,6 +1,6 @@
 #! /bin/env bash
 
 set -e # exit when any command fails
-npm install
-PUBLIC_URL=/vue-element-admin/ npm run build:prod
+
+PUBLIC_URL=/vue-element-admin/ npm install && npm run build:prod
 mv dist ./public/vue-element-admin

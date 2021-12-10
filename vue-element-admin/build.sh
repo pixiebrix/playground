ecd3bd39441c4cfd2ff7502bb89fe6fb4bfb0243
Emmanuel Obua <eobua6882@gmail.com>
2021-12-10 15:54:05 +0300

    Created a new file vue.config.js in the vue-element-admin root directory to modify the one onside the sub module

--- vue-element-admin/build.sh
+++ vue-element-admin/build.sh
@@ -3,6 +3,9 @@
 set -e # exit when any command fails
 
 cd vue-element-admin
+
+cp -R ../vue.config.js vue.config.js
+
 npm install
 PUBLIC_URL=/vue-element-admin npm run build:prod
 mv -f dist ../../public/vue-element-admin

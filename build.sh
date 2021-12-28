fc348ab1256135a3ff9d943c2111f8b46b98b2fa
Federico Brigante <me@fregante.com>
2021-12-28 17:57:49 +0800

    Build gitmodules once

--- build.sh
+++ build.sh
@@ -1,31 +1,4 @@
 #! /bin/bash
-
+# This file is only meant to
 set -e # exit when any command fails
-
-###################################################
-# This script is executed by vercel on deployment #
-###################################################
-
-mkdir -p public
-mv ./-go-to-repo.html public
-
-for DIR in */ ; do
-	if [ "$DIR" = "public/" ]
-	then
-		continue
-	elif [ -f "$DIR/build.sh" ]
-	then (
-		echo "##################"
-		echo "Running /${DIR}build.sh"
-		echo "##################"
-		cd "$DIR"
-		bash ./build.sh
-	)
-	else
-		echo "##################"
-		echo "Moving /$DIR to /public/$DIR unchanged"
-		mkdir -p "public/$DIR"
-		mv "$DIR" public/
-		echo "##################"
-	fi
-done
+cp -r static/* public
--- create-react-app/build.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-set -e # exit when any command fails
-
-yarn
-PUBLIC_URL=/create-react-app/ yarn run build
-mv build ../public/create-react-app
